4a99363d1e7845a6e051fdc827859cc33b1a0167
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2025-10-05 14:57:48 +0530

    commit

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Mausam Bot",
   botOwner: 'Mausam Kar', // Your name
-  ownerNumber: '9186385455784', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '93771101482', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
